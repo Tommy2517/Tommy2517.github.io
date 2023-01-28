d533ac80fdb7c766001dedc4851547aff3b9decc
Tommy2517 <artem07stu@gmail.com>
2023-01-28 20:38:26 +0200

    global refactoring and import new resolves

---
+++ js_lesson1/script.js
@@ -0,0 +1,26 @@
+// let a = 'asd0'
+// let b = 'asd1'
+// let c = 'asd2'
+//
+// let arr = ['hello', 'world', 'okten', c, b, a, [123, 33, 55]];
+// console.log(arr);
+// console.log(arr[2]);
+// console.log(arr[0]);
+// let innerArray = arr[6];
+// console.log(arr[6][2]);
+// console.log(arr[3]);
+//
+// console.log(arr.length)
+//
+// arr[7] = 100;
+// console.log(arr);
+
+
+// let numbers = [];
+// console.log(numbers.length);
+// numbers[numbers.length] = 123;
+// numbers[numbers.length] = 12345;
+// numbers[numbers.length] = 12345;
+// numbers[numbers.length] = 12345;
+// numbers[numbers.length] = 12345;
+// console.log(numbers)
