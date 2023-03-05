6723bde92c841fa796fd12414b9ade2b4d26fe4d
Tommy2517 <artem07stu@gmail.com>
2023-03-05 18:02:10 +0200

    global refactoring and import new resolves update sizing add resolves 3 for js added lesson 10

---
+++ JavaScript/resolves_10/js/main.js
@@ -0,0 +1,67 @@
+// Стоврити форму з трьома полями для name,surname,age та кнопкою. При натисканні на кнопку зчитати данні з полів,
+// та вивести об'єкт в документ. Іншими словами : заповниои форму, натиснули кнопку,
+// під формою з'явився блок з вашим об'єктом
+let form = document.createElement('form');
+form.setAttribute('name','form');
+form.classList.add('form');
+
+let name = document.createElement('input');
+name.setAttribute('name','name');
+
+let surname = document.createElement('input');
+surname.setAttribute('name','surname');
+
+let age = document.createElement('input');
+age.setAttribute('name','age');
+
+let btn = document.createElement('button');
+btn.setAttribute('name','btn');
+btn.innerText = 'Send'
+btn.onclick = function (eo) {
+    eo.preventDefault();
+    let user = document.getElementsByName('form')
+    console.log(user[0].getElementsByTagName('input')[0].value)
+}
+let u = document.querySelector('')
+
+document.body.append(form)
+form.append(name,surname,age, btn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==========================
+// є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код, який при кожному перезавантажені сторінки буде
+// додавати до неї +1
+// ==========================
+// Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessions зберігається
+// інформація про дату та час відвідування сторінки. Є ще сторінка sessions.html (назва довільна), при відвідуванні
+// якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль,
+// а побудувати дом структуру під кожну сессію
+// =========================
+//     зробити масив на 100 об'єктів та дві кнопки prev next
+// при завантажені сторінки з'являються перші 10 об'єктів.
+//     При натисканні next виводяться настпні 10 об'єктів
+// При натисканні prev виводяться попередні 10 об'єктів
+//
+//
+// *** Створити 3 інпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
+//     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
+// (Додатковачастина для завдання)
