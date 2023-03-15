366092b005fa44d838b84efd3f0e5118be5394c6
Tommy2517 <artem07stu@gmail.com>
2023-03-15 02:16:13 +0200

    global refactoring and import new resolves         update sizing         add resolves 3 for js         added lesson 10.         add resolves 10         update resolves 10         add lesson 11         add resolve 12

--- MINI_PROJECT/js/main.js
+++ MINI_PROJECT/js/main.js
@@ -6,12 +6,16 @@ fetch(users)
     .then(users => {
         for (const user of users) {
             let div = document.createElement('div');
-            div.append(user.id + ' ' + user.name)
-            let a = document.createElement('a')
-            a.append(div);
-            a.href = `user_details.html?id=${user.id}`
-            document.body.append(a)
+            div.append(user.name);
+            let a = document.createElement('a');
+            a.append(user.id,div);
+            a.classList.add('user');
+            a.href = `user_details.html?id=${user.id}`;
+
+            document.getElementById('users_s1').append(a)
+            // document.body.append(a);
         }
     })
+
 
 ////////////////////////user_details//////////////////////////////
