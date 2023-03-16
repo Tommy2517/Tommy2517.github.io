1e61017e0c22699a76f0dda105f0ce4a0af3a181
Tommy2517 <artem07stu@gmail.com>
2023-03-16 20:06:10 +0200

    global refactoring and import new resolves         update sizing         add resolves 3 for js         added lesson 10.         add resolves 10         update resolves 10         add lesson 11         add resolve 12

--- MINI_PROJECT/js/UserDetails.js
+++ MINI_PROJECT/js/UserDetails.js
@@ -1,5 +1,7 @@
-let url = new URL(location.href);
-let id = +url.search.replace(/\D+/g,"")
+
+
+let urlUser = new URL(location.href);
+let id = +urlUser.search.replace(/\D+/g,"");
 
 
 let user = new URL('https://jsonplaceholder.typicode.com/users/'+id);
@@ -12,15 +14,48 @@ fetch(user)
             if (typeof obj[key] !== "object") {
                 let underTag = document.createElement(unTag);
                 underTag.append(key, ' - ', obj[key]);
-                tag.append(underTag)
+                tag.append(underTag);
             }
         }
-        return tag
+        return tag;
     }
+
     document.querySelector('.address')
-        .append(reformer(user.address,'ul','li'))
+        .append(reformer(user.address,'ul','li'));
+
+    document.querySelector('.address')
+        .append('geo: ',reformer(user.address.geo,'ul','li'));
+
     document.querySelector('.info')
-        .append(reformer(user,'ul','li'))
+        .append(reformer(user,'ul','li'));
+
     document.querySelector('.company')
-        .append(reformer(user.company,'ul','li'))
+        .append(reformer(user.company,'ul','li'));
 })
+
+let btn_posts = document.getElementById('btn_posts');
+let divPosts = btn_posts.nextElementSibling;
+btn_posts.onclick = function (eo){
+    divPosts.classList.toggle('posts_after');
+}
+
+
+fetch('https://jsonplaceholder.typicode.com/users/'+id+'/posts')
+    .then(posts => posts.json())
+    .then(posts => {
+        for (const post of posts) {
+            let div = document.createElement('div');
+            let p = document.createElement('p')
+            div.append(p)
+            p.append(post.title);
+            div.classList.add('post');
+            divPosts.append(div);
+
+
+            let post_btn = document.createElement('button');
+            post_btn.classList.add('post_btn');
+            post_btn.innerText = 'more'
+
+            div.append(post_btn)
+        }
+    })
