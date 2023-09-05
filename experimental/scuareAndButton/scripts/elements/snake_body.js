913b360ab239337312e513b49f5d555325652b81
Tommy2517 <artem07stu@gmail.com>
2023-09-05 18:48:42 +0300

    remove trash

--- experimental/scuareAndButton/scripts/elements/snake_body.js
+++ experimental/scuareAndButton/scripts/elements/snake_body.js
@@ -4,13 +4,11 @@ body_snake.classList.add('qwe')
 body_snake.setAttribute('id', '1')
 let x_position = 0;
 let y_position = 0;
-let step = 1;
 let top_turn = 0;
 let bot_turn = 0;
 let left_turn = 0;
 let right_turn = 0;
 let intervalID = null;
-let animationIsRunning = false;
 
 document.body.onkeydown = function (eo) {
     if (eo.key === 'd' && right_turn === 0) {
@@ -82,46 +80,4 @@ document.body.onkeydown = function (eo) {
     }
 }
 
-
-// for (;x_position < 180; x_position+=10){
-//     if (x_position >180){
-//         x_position =0
-//     }
-// }
-
-// document.body.onkeydown = function (eo) {
-//     if (eo.key === 'd') {
-//
-//         x_position += step;
-//         if (x_position > 180) {
-//             x_position = 0;
-//         }
-//         body_snake.style.left = `${x_position}px`
-//     }
-//
-//     if (eo.key === 'a') {
-//         x_position -= step;
-//         if (x_position < 0) {
-//             x_position = 180;
-//         }
-//         body_snake.style.left = `${x_position}px`
-//     }
-//
-//     if (eo.key === 's') {
-//         y_position += step;
-//         if (y_position > 80) {
-//             y_position = 0;
-//         }
-//         body_snake.style.top = `${y_position}px`
-//     }
-//
-//     if (eo.key === 'w') {
-//         if (y_position < 1) {
-//             y_position = 81;
-//         }
-//         y_position -= step;
-//         body_snake.style.top = `${y_position}px`
-//     }
-//     console.log(eo.key)
-// }
 document.body.append(body_snake)
