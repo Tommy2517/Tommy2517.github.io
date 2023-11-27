bfa92f7647004aceef920cb6556d4a1f9d2d4518
Tommy2517 <artem07stu@gmail.com>
2023-11-27 20:50:40 +0200

    add game wordColors

--- experimental/games/wordsColor/main.js
+++ experimental/games/wordsColor/main.js
@@ -1,5 +1,5 @@
-const colorNames = ['yellow', 'red', 'blue', 'black'];
-const colorValues = ['yellow', 'red', 'blue', 'black'];
+const colorValues = [{name: 'yellow'}, {name: 'red'}, {name: 'blue'}, {name: 'black'}];
+let score = 0;
 
 const div = document.createElement('div')
 div.classList.add('block')
@@ -11,30 +11,40 @@ btnYes.textContent = 'Yes'
 const btnNo = document.createElement('button')
 btnNo.textContent = 'No'
 
-let score = 0;
 const divScore = document.createElement('div')
 divScore.classList.add('block')
 divScore.textContent = `${score}`
+
+let rand1 = null;
+let rand2 = null;
+let rand3 = null;
 const change = () => {
-    // div.style.color = colorValues[Math.floor(Math.random() * 4)]
-    div.textContent = colorValues[Math.floor(Math.random() * 4)]
-    // console.log('div 1 - ', div.style.background)
-    div2.style.color = colorValues[Math.floor(Math.random() * 4)]
-    div2.textContent = colorValues[Math.floor(Math.random() * 4)]
-    console.log('div 2 - ', div2.style.background)
+    rand1 = Math.floor(Math.random() * 4);
+    rand2 = Math.floor(Math.random() * 4);
+    rand3 = Math.floor(Math.random() * 4);
+
+    div.textContent = colorValues[rand1].name
+    div2.style.color = colorValues[rand2].name
+    div2.textContent = colorValues[rand3].name
+
     divScore.textContent = `${score}`
 }
+change()
 
 btnNo.onclick = () => {
-    if (div.textContent !== div2.style.color){
+    if (rand1 !== rand2){
         score++
     }
     change()
 }
 btnYes.onclick = () => {
-    if (div.textContent === div2.style.color){
+    if (rand1 === rand2){
         score++
     }
     change()
 }
-document.body.append(div, div2, btnYes,btnNo,divScore)
+const main = document.getElementById('main')
+const answer = document.getElementById('answer')
+answer.append(btnYes,btnNo)
+main.append(div, div2, answer, divScore)
+document.body.append(main)
